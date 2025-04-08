d645a261db67ec41327ffd519a319278d792c32c
SantaMcCloud <santino_faack@gmx.de>
2025-04-08 20:45:15 +0200

    change setup

--- setup.py
+++ setup.py
@@ -2,14 +2,18 @@ from setuptools import setup, find_packages
 
 ## install main application
 desc = "utility script for creating data files for statistic based on kmers"
+
+with open("README.md", "r", encoding = "utf-8") as fh:
+    long_description = fh.read()
 setup(
     name="kmer2stats",
     version='1.0.0',
     description=desc,
-    long_description=desc + "\n See README for more information.",
+    long_description=long_description,
+    long_description_content_type = "text/markdown",
     author="Santino Faack",
     author_email="santino_faack@gmx.de",
-    license="MIT license",
+    license="MIT",
     packages=find_packages(),
     url="https://github.com/SantaMcCloud/kmer2stats",
     scripts=[
