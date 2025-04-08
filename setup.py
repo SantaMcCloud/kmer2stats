a2ecf2b07b1d1205455ff70ca196bb44fdd3589b
SantaMcCloud <santino_faack@gmx.de>
2025-04-08 22:48:12 +0200

    last commit for release for pypi

--- setup.py
+++ setup.py
@@ -5,9 +5,19 @@ desc = "utility script for creating data files for statistic based on kmers"
 
 with open("README.md", "r", encoding = "utf-8") as fh:
     long_description = fh.read()
+
+import os
+lib_folder = os.path.dirname(os.path.realpath(__file__))
+requirement_path = f"{lib_folder}/requirements.txt"
+install_requires = []
+if os.path.isfile(requirement_path):
+    with open(requirement_path) as f:
+        install_requires = f.read().splitlines()
+
 setup(
     name="kmer2stats",
     version='1.0.0',
+    install_requires=install_requires,
     description=desc,
     long_description=long_description,
     long_description_content_type = "text/markdown",
