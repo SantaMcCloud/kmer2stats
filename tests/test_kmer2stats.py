5b8f88b6abe9c4fb6ecd60a36993b31cd7742f56
SantaMcCloud <santino_faack@gmx.de>
2026-04-27 20:25:37 +0200

    add unit test and GitHub test and rename some files

---
+++ tests/test_kmer2stats.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from kmer2stats import compute_stats_from_counts
+
+
+def test_kmer_file_input(tmp_path):
+    # 1. Create a temporary file
+    file_path = tmp_path / "kmers.txt"
+
+    # 2. Write test data
+    file_path.write_text("""
+AAAAAC 6870
+AAAAAG 6312
+AAAAAT 7966
+AAAACA 5133
+AAAACC 5600
+AAAACG 5870
+AAAACT 3911
+""")
+
+    # 3. Run function
+    result = compute_stats_from_counts(str(file_path))
+
+    # 4. Assertions
+    assert isinstance(result, pd.DataFrame)
+    # check values properly
+    assert "shannon" in result.index
+    assert result.loc["num_singletons"].values[0] == 0
